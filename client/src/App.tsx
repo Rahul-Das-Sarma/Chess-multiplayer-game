68a78acedda2b0f593dc44401a3efd14d0064d11
Rahul-Das-Sarma <rahuldas35748@gmail.com>
2024-10-21 22:25:33 +0530

    feat:Initialize project

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import ChessBoard from "./components/Chessboard";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-[80vh]">
+      <ChessBoard />
+    </div>
+  );
+}
+
+export default App;
